c580669a1834b8ddbac9c3114d7b74378aca86bf
changquan.cui <ccq1170@gmail.com>
2023-01-22 15:52:24 +0900

    移行 dein->lua

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -3,5 +3,30 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'skanehira/gh.vim'
+  use 'mattesgroeger/vim-bookmarks'
+  use 'vim-airline/vim-airline-themes'
+  use 'mattn/webapi-vim'
+  use 'tpope/vim-surround'
+  use 'vim-scripts/yanktmp.vim'
+  -- # 検索用の素晴らしいプラグイン :S/検索キーワード :%S/xxx/yyy 置換
+  use 'tpope/vim-abolish'
+  use 'thinca/vim-visualstar'
+  use 'tmhedberg/matchit'
+  use 'tpope/vim-unimpaired'
+  use 'vim-ruby/vim-ruby'
+  use 'kana/vim-textobj-user'
+  use 'rhysd/vim-textobj-ruby'
+  use 'tpope/vim-rbenv'
+  use 'tpope/vim-bundler'
+
+  -- Lazy loading:
+  -- Load on specific commands
+
+  -- Gbrowse to open Bitbucket URL
+  use {'tommcdo/vim-fubitive', opt = true, cmd = {'Gbrowse'}}
+
+  -- Gbrowse to open Github URL
+  use {'tpope/vim-rhubarb', opt = true, cmd = {'Gbrowse'}}
 
 end)
