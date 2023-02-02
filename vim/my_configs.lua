a583fd5822858969ad347fa494f26c9f0a4448a9
changquan.cui <ccq1170@gmail.com>
2023-02-02 16:35:06 +0900

    save

--- vim/my_configs.lua
+++ vim/my_configs.lua
@@ -1,53 +1,45 @@
+-- Option系 --
+vim.o.timeout = 'timeoutlen=500 ttimeoutlen=50' -- https://yukidarake.hateblo.jp/entry/2015/07/10/201356
+vim.o.foldmethod = 'manual'                     -- https://qiita.com/izumin5210/items/7e0ad2f86d0686d8b376
+vim.o.nocompatible = true                       -- We're running Vim, not Vi!
+vim.o.number = true
+vim.o.termencoding = 'utf-8'
+vim.o.encoding = 'utf-8'                        -- 文字コードの設定
+vim.o.fileencodings = 'utf-8'                   -- fileencodingsの設定ではencodingの値を一番最後に記述する
+vim.o.fileformats = 'unix,dos,mac'
+vim.o.hlsearch = true                           -- 検索文字をハイライト
+vim.o.smartcase = true                          -- 大文字小文字無視, 大文字ではじめたら大文字小文字無視しない
+vim.o.laststatus = '2'                          -- 常にステータスラインを表示
+vim.o.tabstop = '2'                             -- ファイル内の <Tab> が対応する空白の数。
+vim.o.shiftwidth = '2'                          -- 自動インデントの各段階に使われる空白の数。
+vim.o.expandtab = true                          -- Insertモードで: <Tab>
+vim.o.hidden = true                             -- 保存されていないファイルがあるときでも別のファイルを開くことが出来る
+vim.o.wildmenu = true                           -- コマンドライン補完を拡張モードにする
+vim.o.wildmode = 'list:longest,full'
+vim.o.backspace = 'indent,eol,start'            -- BSで削除: indentは行頭の空白,eolは改行,startは挿入モード開始位置より手前の文字
+vim.o.cursorline = true                         -- カーソル行の強調表示
+vim.o.mouse = 'a'
+vim.o.notagbsearch = true
+vim.o.ignorecase = true
+vim.o.ruler = true
+vim.o.virtualedit = 'block'
+vim.o.nowrap = true                             -- 画面の端で行を折り返すのをやめる方法
+vim.o.clipboard = 'unnamedplus'
+
+-- vim.o.autoindent = true                      -- 新しい行を開始したときに、新しい行のインデントを現在行と同じ量にする。
+-- vim.o.nows = true                            -- 文字列検索の際、ファイルの終端から先頭に戻らない
+-- vim.o.relativenumber = false                 -- 行番号を相対表示にする、移動しやすくするため
+-- insert mode で deleteを有効にする
+-- https://sseze.hatenablog.com/entry/20120414/1334398422
+-- カーソルを行頭，行末で止まらないようにする
+-- vim.o.whichwrap = 'b,s,h,l,<,>,[,]'
+
 vim.cmd([[
   colorscheme elflord
-  " https://yukidarake.hateblo.jp/entry/2015/07/10/201356
-  set timeout timeoutlen=500 ttimeoutlen=50
-
-  " https://qiita.com/izumin5210/items/7e0ad2f86d0686d8b376
-  " rubyの時、vimが重い
-  set foldmethod=manual
-
   syntax on
   filetype on            " ファイル形式の検出を有効化
   filetype indent on    " Enable filetype-specific indenting
   filetype plugin on    " Enable filetype-specific plugins
-
-  " -- Option系 --
-  " set nows             " 文字列検索の際、ファイルの終端から先頭に戻らない
-  set number
-  " set relativenumber     " 行番号を相対表示にする、移動しやすくするため
-  set nocompatible      " We're running Vim, not Vi!
-  set termencoding=utf-8
-  set encoding=utf-8     " 文字コードの設定
-  set fileencodings=utf-8 " fileencodingsの設定ではencodingの値を一番最後に記述する
-  set fileformats=unix,dos,mac
-  set hlsearch           " 検索文字をハイライト
-  set smartcase          " 大文字小文字無視, 大文字ではじめたら大文字小文字無視しない
-  set laststatus=2       " 常にステータスラインを表示
-  set tabstop=2          " ファイル内の <Tab> が対応する空白の数。
-  set shiftwidth=2       " 自動インデントの各段階に使われる空白の数。
-  set expandtab          " Insertモードで: <Tab>
-  " set autoindent       " 新しい行を開始したときに、新しい行のインデントを現在行と同じ量にする。
-  set hidden             " 保存されていないファイルがあるときでも別のファイルを開くことが出来る
-  set wildmenu           " コマンドライン補完を拡張モードにする
-  set wildmode=list:longest,full
-  "BSで削除できるものを指定する
-  " indent  : 行頭の空白
-  " eol     : 改行
-  " start   : 挿入モード開始位置より手前の文字
-  set backspace=indent,eol,start
-  set cursorline         " カーソル行の強調表示
-  set mouse=a
-  set notagbsearch
-  set ignorecase
-  set ruler
-  set virtualedit=block
-  set nowrap              " 画面の端で行を折り返すのをやめる方法
-
-  " insert mode で deleteを有効にする
-  " https://sseze.hatenablog.com/entry/20120414/1334398422
-  "カーソルを行頭，行末で止まらないようにする
-  " set whichwrap=b,s,h,l,<,>,[,]
 
   " -- map系 --
   " <Del> key: ノーマルモードで数値 (count) を入力しているときは、数字の最後の桁を削除します。
@@ -105,7 +97,6 @@ vim.cmd([[
   " https://yuheikagaya.hatenablog.jp/entry/2012/12/03/202556
   nnoremap gs :vertical wincmd f<CR>
 
-  set clipboard=unnamedplus
   " Make Terminal default mode insert
   " https://github.com/neovim/neovim/issues/8816
   autocmd TermOpen term://* startinsert
