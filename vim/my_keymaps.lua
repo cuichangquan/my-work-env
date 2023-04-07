ffe21fc3988626e4133247cb419c294ced8ab414
changquan.cui <ccq1170@gmail.com>
2023-04-07 17:55:54 +0900

    save

--- vim/my_keymaps.lua
+++ vim/my_keymaps.lua
@@ -100,6 +100,12 @@ keymap("n", "p", "p`]", { noremap = true, silent = true })
 keymap("n", "<C-s>", ":call Open_existed_vifm_buffer()<CR>", { noremap = true })
 keymap("n", "<C-p>", ":<C-u>FZF<CR>", { noremap = true })
 
+-- " https://qiita.com/ymiyamae/items/cea5103c65184f55d62e
+-- "挿入モードのときに hl でカーソルを移動する
+keymap("i", "<C-h>", "<Left>", { noremap = true })
+keymap("i", "<C-l>", "<Right>", { noremap = true })
+
+
 vim.cmd([[
   " -- map系 --
   " <Del> key: ノーマルモードで数値 (count) を入力しているときは、数字の最後の桁を削除します。
@@ -112,11 +118,6 @@ vim.cmd([[
   tnoremap <Esc><Esc><Esc> <C-\><C-n>:q!<CR>
   command! Lazygit tabnew term://lazygit
   autocmd TermClose term://.//*:lazygit* bprevious | bwipeout!
-
-  " https://qiita.com/ymiyamae/items/cea5103c65184f55d62e
-  "挿入モードのときに hl でカーソルを移動する
-  inoremap <C-h> <Left>
-  inoremap <C-l> <Right>
 
   " コマンド履歴Windowはうるさいから、:qに置き換える
   " map q: :q
