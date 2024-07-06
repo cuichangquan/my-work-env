35dcac91039227da6bb8ab8c40022e468b83d207
changquan.cui <ccq1170@gmail.com>
2024-07-06 21:12:30 +0900

    save

--- vim/my_options.lua
+++ vim/my_options.lua
@@ -1,17 +1,16 @@
 -- Option系 --
-vim.o.timeout = 'timeoutlen=500 ttimeoutlen=50' -- https://yukidarake.hateblo.jp/entry/2015/07/10/201356
+vim.o.timeoutlen = 500                          -- https://yukidarake.hateblo.jp/entry/2015/07/10/201356
+vim.o.ttimeoutlen = 50                          -- https://yukidarake.hateblo.jp/entry/2015/07/10/201356
 vim.o.foldmethod = 'manual'                     -- https://qiita.com/izumin5210/items/7e0ad2f86d0686d8b376
-vim.o.nocompatible = true                       -- We're running Vim, not Vi!
 vim.o.number = true
-vim.o.termencoding = 'utf-8'
 vim.o.encoding = 'utf-8'                        -- 文字コードの設定
 vim.o.fileencodings = 'utf-8'                   -- fileencodingsの設定ではencodingの値を一番最後に記述する
 vim.o.fileformats = 'unix,dos,mac'
 vim.o.hlsearch = true                           -- 検索文字をハイライト
 vim.o.smartcase = true                          -- 大文字小文字無視, 大文字ではじめたら大文字小文字無視しない
-vim.o.laststatus = '2'                          -- 常にステータスラインを表示
-vim.o.tabstop = '2'                             -- ファイル内の <Tab> が対応する空白の数。
-vim.o.shiftwidth = '2'                          -- 自動インデントの各段階に使われる空白の数。
+vim.o.laststatus = 2                            -- 常にステータスラインを表示
+vim.o.tabstop = 2                               -- ファイル内の <Tab> が対応する空白の数。
+vim.o.shiftwidth = 2                            -- 自動インデントの各段階に使われる空白の数。
 vim.o.expandtab = true                          -- Insertモードで: <Tab>
 vim.o.hidden = true                             -- 保存されていないファイルがあるときでも別のファイルを開くことが出来る
 vim.o.wildmenu = true                           -- コマンドライン補完を拡張モードにする
@@ -19,7 +18,6 @@ vim.o.wildmode = 'list:longest,full'
 vim.o.backspace = 'indent,eol,start'            -- BSで削除: indentは行頭の空白,eolは改行,startは挿入モード開始位置より手前の文字
 vim.o.cursorline = true                         -- カーソル行の強調表示
 vim.o.mouse = 'a'
-vim.o.notagbsearch = true
 vim.o.ignorecase = true
 vim.o.ruler = true
 vim.o.virtualedit = 'block'
